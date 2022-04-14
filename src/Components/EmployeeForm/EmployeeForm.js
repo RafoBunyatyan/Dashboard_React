d84dcb9c66247899808d0f730d9a9b80e960ab81
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-04-14 22:41:33 +0400

    add modals @media, add localStroage

--- src/Components/EmployeeForm/EmployeeForm.js
+++ src/Components/EmployeeForm/EmployeeForm.js
@@ -2,42 +2,73 @@ import React, { useEffect, useState } from "react";
 import "./EmployeeForm.css";
 import { Button, Form, Modal } from "react-bootstrap";
 
+function EmployeeForm(props) {
+	const [value, setValue] = useState("")
+	const [value1, setValue1] = useState("")
+	const [value2, setValue2] = useState("")
+	const [value3, setValue3] = useState("")
+	const [value4, setValue4] = useState("")
 
-function EmployeeForm(props){
+	useEffect(() => {
+		localStorage.setItem('value', JSON.stringify(value))
+	}, [value])
 
+	useEffect(() => {
+		localStorage.setItem('value1', JSON.stringify(value1))
+	}, [value1])
 
-	const [show, setShow] = useState(false);
+	useEffect(() => {
+		localStorage.setItem('value2', JSON.stringify(value2))
+	}, [value2])
 
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
+	useEffect(() => {
+		localStorage.setItem('value3', JSON.stringify(value3))
+	}, [value3])
 
+	useEffect(() => {
+		localStorage.setItem('value4', JSON.stringify(value4))
+	}, [value4])
+
+	// const [change, setChange] = useState([
+	// 	{ id: 1, FULLNAME: "", BIRTHDAY: "", GENDER: "", NITIONALITY: "", ADDRESS: "" }
+	// ]);
+
+	// const addEmployee = (FULLNAME, BIRTHDAY, GENDER, NITIONALITY, ADDRESS) => {
+	// 	setChange([...change, { id: 1, FULLNAME, BIRTHDAY, GENDER, NITIONALITY, ADDRESS }])
+	// }
 
 	return (
 		<>
-			<Button variant="primary" onClick={handleShow}>
-			Open Form
-			</Button>
-
-			<Modal show={show} onHide={handleClose} >
+			<Modal show={props.showForm} onHide={props.handleFormClose} >
 				<Modal.Header closeButton></Modal.Header>
 				<Modal.Body>
 					<Form>
 						<Form.Group className="Form_Grup" controlId="exampleForm.ControlInput1">
-							<Form.Label className="Label">asd  </Form.Label>
-							<Form.Control type="text" required autoFocus />
-							<Form.Label className="Label">BIRTHDAY  </Form.Label>
-							<Form.Control type="text" required autoFocus />
-							<Form.Label className="Label">GENDER  </Form.Label>
-							<Form.Control type="text" required autoFocus />
-							<Form.Label className="Label">NITIONALITY  </Form.Label>
-							<Form.Control type="text" required autoFocus />
-							<Form.Label className="Label">ADDRESS  </Form.Label>
-							<Form.Control type="text" required autoFocus />
+							<Form.Label className="Label">FULLNAME</Form.Label>
+							<Form.Control type="text" autoFocus onChange={(evt) => {
+								setValue(evt.target.value)
+							}} />
+							<Form.Label className="Label">BIRTHDAY</Form.Label>
+							<Form.Control type="text" autoFocus onChange={(evt) => {
+								setValue1(evt.target.value)
+							}} />
+							<Form.Label className="Label">GENDER</Form.Label>
+							<Form.Control type="text" autoFocus onChange={(evt) => {
+								setValue2(evt.target.value)
+							}} />
+							<Form.Label className="Label">NITIONALITY</Form.Label>
+							<Form.Control type="text" autoFocus name="value" onChange={(evt) => {
+								setValue3(evt.target.value)
+							}} />
+							<Form.Label className="Label">ADDRESS</Form.Label>
+							<Form.Control type="text" autoFocus onChange={(evt) => {
+								setValue4(evt.target.value)
+							}} />
 						</Form.Group>
 					</Form>
 				</Modal.Body>
 				<Modal.Footer>
-					<Button className="button_borders" variant="primary" onClick={() => console.log("event")}>
+					<Button className="button_borders" variant="primary" onClick={props.handleFormClose}>
 						Save Changes
 					</Button>
 				</Modal.Footer>
@@ -47,9 +78,3 @@ function EmployeeForm(props){
 };
 
 export default EmployeeForm;
-
-
-
-
-
-
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,28 +3,45 @@ import Icon, { Stack } from '@mdi/react';
 import { mdiAccount, mdiPencil, mdiPoll } from '@mdi/js';
 import "./Header.css";
 import EmployeeForm from "../EmployeeForm/EmployeeForm";
-import EmployeeAbout from "../EmployeeForm/EmployeeAbout";
+import EmployeeAbout from "../EmployeeAbout/EmployeeAbout";
+
 
 function Header() {
+	const [showForm, setShowForm] = useState(false);
+	const handleFormClose = () => setShowForm(false);
+	const handleFormShow = () => setShowForm(true);
+
+	const [showAbout, setShowAbout] = useState(false);
+	const handleAboutClose = () => setShowAbout(false);
+	const handleAboutShow = () => setShowAbout(true);
+
+
 	// const [change, setChange] = useState([
-	// 	{ id: uuidv4(), FULLNAME: "", BIRTHDAY: "", GENDER: "", NITIONALITY: "", ADDRESS: ""  }
+	// 	{ id: 1, FULLNAME: "", BIRTHDAY: "", GENDER: "", NITIONALITY: "", ADDRESS: "" }
 	// ]);
 
 	// useEffect(() => {
 	// 	setChange(JSON.parse(localStorage.getItem('change')))
 	// }, [])
 
-
 	// useEffect(() => {
 	// 	localStorage.setItem('change', JSON.stringify(change));
 	// })
 
-	// cons t addEmployee = (FULLNAME, BIRTHDAY, GENDER, NITIONALITY, ADDRESS) => {
-	// 	setChange([...change, { id: uuidv4(), FULLNAME, BIRTHDAY, GENDER, NITIONALITY, ADDRESS  }])
+	// const addEmployee = (FULLNAME, BIRTHDAY, GENDER, NITIONALITY, ADDRESS) => {
+	// 	setChange([...change, { id: 1, FULLNAME, BIRTHDAY, GENDER, NITIONALITY, ADDRESS }])
 	// }
-	
+
+	let a = JSON.parse(localStorage.getItem('value'));
+	let b = JSON.parse(localStorage.getItem('value1'));
+	let c = JSON.parse(localStorage.getItem('value2'));
+	let d = JSON.parse(localStorage.getItem('value3'));
+	let e = JSON.parse(localStorage.getItem('value4'));
+
 	return (
-		<>	
+		<>
+			<EmployeeForm showForm={showForm} handleFormClose={handleFormClose} />
+			<EmployeeAbout showAbout={showAbout} handleAboutClose={handleAboutClose} />
 			<div className="header">
 				<div className="header_blockUser">
 					<div className="header_personalEdit">
@@ -32,18 +49,17 @@ function Header() {
 							<Icon className="IconMdi" path={mdiAccount}></Icon>
 							<span className="personalText">Personal Information</span>
 						</div>
-						{/* <a className="personal_Edit" title="Edit" onClick={handleOpenFormModal}>
+						<a className="personal_Edit" title="Edit" onClick={handleFormShow}>
 							<Icon className="IconMdi" path={mdiPencil}></Icon>
-						</a> */}
-						<EmployeeForm/>
+						</a>
 					</div>
 					<div className="personal_Information">
 						<ul className="personal_grup">
-							<li className="personal_Item">fullname {  }</li>
-							<li className="personal_Item">birthday</li>
-							<li className="personal_Item">gender</li>
-							<li className="personal_Item">nitionality</li>
-							<li className="personal_Item">address</li>
+							<li className="personal_Item">fullname {a}</li>
+							<li className="personal_Item">birthday{b}</li>
+							<li className="personal_Item">gender{c}</li>
+							<li className="personal_Item">nitionality{d}</li>
+							<li className="personal_Item">address{e}</li>
 						</ul>
 					</div>
 				</div>
@@ -53,24 +69,23 @@ function Header() {
 							<Icon className="IconPoll" path={mdiPoll}></Icon>
 							<span className="personalText">About My</span>
 						</div>
-						{/* <a className="personal_Edit" title="Edit" onClick={handleOpenAboutModal}>
+						<a className="personal_Edit" title="Edit" onClick={handleAboutShow}>
 							<Icon className="IconMdi" path={mdiPencil}></Icon>
-						</a> */}
-						<EmployeeAbout/>
+						</a>
 					</div>
 					<div className="personal_Information">
 						<ul className="personal_grup">
-								<li className="personal_Item">employment</li>
-								<li className="personal_Item">education</li>
-								<li className="personal_Item">skills</li>
-								<li className="personal_Item">language</li>
-								<li className="personal_Item">hobbies</li>
+							<li className="personal_Item">employment</li>
+							<li className="personal_Item">education</li>
+							<li className="personal_Item">skills</li>
+							<li className="personal_Item">language</li>
+							<li className="personal_Item">hobbies</li>
 						</ul>
 					</div>
 				</div>
 			</div>
 		</>
-	);
-};
+	)
+}
 
 export default Header;
